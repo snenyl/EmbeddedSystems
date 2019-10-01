bae7dd1617611bde50c01e5dafdd86189ba8507d
orwah <orwah_92@hotmail.com>
2019-10-01 13:03:51 +0200

    pex1_as3 lastet opp

---
+++ pex1_as3/GccApplication1/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 01.10.2019 11:43:56
+ * Author : orwah
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	
+	DDRC |= 1 << PC3;    
+	PORTC &= ~(1 << PC3); 
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
